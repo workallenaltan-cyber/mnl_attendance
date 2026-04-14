f4d6061e9688222ec143ab226e323609794a3171
workallenaltan <workallenaltan@gmail.com>
2026-04-15 00:16:25 +0800

    update

--- backend/routes/attendance.js
+++ backend/routes/attendance.js
@@ -165,7 +165,7 @@ router.post("/check", verify, async (req, res) => {
 		matchedCompany = c;
 	  }
 	}
-	const nearCompany = matchedCompany.company_name;
+	const nearCompany = matchedCompany.company_code;
 	// =============================
 	// ❌ 不在任何分行范围
 	// =============================
